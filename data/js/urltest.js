fc0710b2f7cfcf1a549f1451df529cbc8de8b5a1
Steve Fink <sfink@mozilla.com>
2011-02-17 13:38:45 -0800

    Apply tweaks to landfill (test) bugzilla installations at bugzilla.org.

--- data/js/urltest.js
+++ data/js/urltest.js
@@ -1,3 +1,4 @@
 function onBugzillaPage(url) {
-  return /https:\/\/bugzilla(-[a-zA-Z]+)*\.mozilla\.org/.test(url);
+  return /https:\/\/bugzilla(-[a-zA-Z]+)*\.mozilla\.org/.test(url)
+      || /https:\/\/landfill.*\.bugzilla\.org/.test(url);
 }

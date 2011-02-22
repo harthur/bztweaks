99fc41e4c0cf07af51337403aa17085ca94d8928
Ehsan Akhgari <ehsan@mozilla.com>
2011-02-22 00:45:10 -0500

    More whitespace adjustments

--- data/js/bug-page-mod.js
+++ data/js/bug-page-mod.js
@@ -1156,42 +1156,42 @@ function htmlEncode(str) {
 }
 
 function addNewLinks(d) {
-    var product = d.querySelector("#field_container_product option[selected]");
-    var component = d.querySelector("#component option[selected]");
+  var product = d.querySelector("#field_container_product option[selected]");
+  var component = d.querySelector("#component option[selected]");
 
-    if (product) {
-      var label = d.getElementById('field_container_product');
-      var url = 'enter_bug.cgi?product=' + encodeURIComponent(product.value);
-      span.innerHTML = "(<a href='" + url + "'>new</a>)";
-      if (label) {
-        label.appendChild(d.createTextNode("("));
-        var link = d.createElement('a');
-        link.href = url;
-        link.textContent = "new";
-        link.title = "File a new bug in the same Product";
-        var span = d.createElement('span');
-        span.appendChild(link);
-        label.appendChild(span);
-        label.appendChild(d.createTextNode(")"));
-      }
+  if (product) {
+    var label = d.getElementById('field_container_product');
+    var url = 'enter_bug.cgi?product=' + encodeURIComponent(product.value);
+    span.innerHTML = "(<a href='" + url + "'>new</a>)";
+    if (label) {
+      label.appendChild(d.createTextNode("("));
+      var link = d.createElement('a');
+      link.href = url;
+      link.textContent = "new";
+      link.title = "File a new bug in the same Product";
+      var span = d.createElement('span');
+      span.appendChild(link);
+      label.appendChild(span);
+      label.appendChild(d.createTextNode(")"));
     }
+  }
 
-    if (product && component) {
-      var select = d.querySelector("select#component");
-      var label = select.parentNode;
-      var url = 'enter_bug.cgi?product=' + encodeURIComponent(product.value) + '&component=' + encodeURIComponent(component.value);
-      if (label) {
-        label.appendChild(d.createTextNode("("));
-        var link = d.createElement('a');
-        link.href = url;
-        link.textContent = "new";
-        link.title = "File a new bug in the same Product and Component";
-        var span = d.createElement('span');
-        span.appendChild(link);
-        label.appendChild(span);
-        label.appendChild(d.createTextNode(")"));
-      }
+  if (product && component) {
+    var select = d.querySelector("select#component");
+    var label = select.parentNode;
+    var url = 'enter_bug.cgi?product=' + encodeURIComponent(product.value) + '&component=' + encodeURIComponent(component.value);
+    if (label) {
+      label.appendChild(d.createTextNode("("));
+      var link = d.createElement('a');
+      link.href = url;
+      link.textContent = "new";
+      link.title = "File a new bug in the same Product and Component";
+      var span = d.createElement('span');
+      span.appendChild(link);
+      label.appendChild(span);
+      label.appendChild(d.createTextNode(")"));
     }
+  }
 }
 
 function tbplbotSpamCollapser(d) {
